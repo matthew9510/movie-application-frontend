2c6c087fd88c3db8567b59ca997c7b53fba190ca
Matthew <matthew9510@gmail.com>
2019-08-19 21:29:55 -0700

    Added Register component to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home', component: HomeComponent},
+  {path:'register', component:RegisterComponent},
   {path:'**', component: HomeComponent}
 ];
 
